845273ef5350b81559084afb57a2d7479684a307
Sebastian During <colaholiker@waskurzes.de>
2022-08-22 13:49:26 +0200

    cron

--- wq-v/php_opcache.php
+++ wq-v/php_opcache.php
@@ -1,8 +1,8 @@
-mem_used.value 72413416
-mem_free.value 420293920
-mem_wasted.value 44163576
+mem_used.value 72725408
+mem_free.value 395963744
+mem_wasted.value 68181760
 hitrate.value 99.99
-hits.value 48345235
-misses.value 5177
-used_keys.value 5236
-free_keys.value 10993
+hits.value 74996191
+misses.value 6210
+used_keys.value 5249
+free_keys.value 10980
