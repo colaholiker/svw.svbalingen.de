9f1dcce7a5760807c8bdab0b3abb4cde2afba9c3
Sebastian During <colaholiker@waskurzes.de>
2023-12-11 19:46:55 +0100

    cron

--- wq-v/php_opcache.php
+++ wq-v/php_opcache.php
@@ -1,8 +1,8 @@
-mem_used.value 68787952
-mem_free.value 18563240
-mem_wasted.value 46866536
-hitrate.value 100
-hits.value 237668363
-misses.value 5255
-used_keys.value 5003
-free_keys.value 11226
+mem_used.value 66851040
+mem_free.value 56308256
+mem_wasted.value 11058432
+hitrate.value 99.97
+hits.value 10494881
+misses.value 3147
+used_keys.value 4571
+free_keys.value 11658
